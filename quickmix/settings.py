da7173a526f3e6d8221564732a7a8b0185fa2cb6
Rgarner1008 <71299429+Rgarner1008@users.noreply.github.com>
2020-11-28 13:05:05 -0500

    tst

--- quickmix/settings.py
+++ quickmix/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+<<<<<<< HEAD
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+=======
+
+>>>>>>> parent of 143c5c6... fixing heroku
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
